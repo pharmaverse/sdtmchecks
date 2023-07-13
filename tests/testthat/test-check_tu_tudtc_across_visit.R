61c74c49b6035a93b850c36eda262a94cf6eace5
Wang, Laura {TBB~SOUTH SAN FRANCISCO} <wang.laura@gene.com>
2023-07-14 00:26:29 +0200

    Test `check_tu_tudtc_across_visit`

---
+++ tests/testthat/test-check_tu_tudtc_across_visit.R
@@ -0,0 +1,34 @@
+test_that("Returns true when no errors present", {
+  
+  TU <- data.frame(USUBJID = 1,
+                   TUDTC = c(rep("2016-01-01",3), rep("2016-06-01",3), rep("2016-06-24",4)),
+                   VISIT = c(rep("C1D1",3), rep("C1D2",3), rep("C2D1",4)),
+                   stringsAsFactors=FALSE)
+  
+  expect_true(check_tu_tudtc_across_visit(TU))
+  
+}) 
+
+test_that("Returns false when errors present", {
+  
+  TU <- data.frame(USUBJID = 1,
+                   TUDTC = c(rep("2016-01-01",3), rep("2016-06-01",5), rep("2016-06-24",2)),
+                   VISIT = c(rep("C1D1",3), rep("C1D2",3), rep("C2D1",4)),
+                   stringsAsFactors=FALSE)
+  
+  expect_false(check_tu_tudtc_across_visit(TU))
+  
+})
+
+test_that("Returns false when expected column not present", {
+  
+  TU <- data.frame(USUBJID = 1,
+                   TUDTC = c(rep("2016-01-01",3), rep("2016-06-01",5), rep("2016-06-24",2)),
+                   VISIT = c(rep("C1D1",3), rep("C1D2",3), rep("C2D1",4)),
+                   stringsAsFactors=FALSE)
+  
+  TU$USUBJID <- NULL
+  
+  expect_false(check_tu_tudtc_across_visit(TU))
+  
+})
