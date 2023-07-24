124a7749ec23647460e3695b4273d839a909ef73
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2023-07-25 00:38:11 +0200

    remove shiny code - website already has report

--- inst/dev/sdtmchecks_covr_report.R
+++ inst/dev/sdtmchecks_covr_report.R
@@ -2,12 +2,11 @@
 # Save html file and metadata about report run
 # Run Shiny app based on html 
 
-
 options(covr.record_tests = TRUE) 
 
 # create HTML report of unit test coverage from {covr}
 html_rpt_name <- "sdtmchecks_covr_report.html"
-x <- covr::package_coverage()
+
 html_loc_rpt <- here::here("inst", "dev", html_rpt_name)
 
 # save metadata - create as a markdown file 
@@ -19,6 +18,8 @@ html_rpt_run_meta <- data.frame(
 )
 
 
+
+x <- covr::package_coverage()
 covr::report(
   x = x
   ,file = html_loc_rpt
@@ -26,31 +27,4 @@ covr::report(
 )
 
 # save metadata from report run as html_rpt_meta.RData in sdtmchecks/inst/dev/lib
-save(html_rpt_run_meta, file = here::here("inst", "dev", "lib", "html_rpt_run_meta.RData"), version=2)
-     
-
-# report as Shiny App based on report
-library(shiny) 
-
-meta <- html_rpt_run_meta
-
-ui <- fluidPage(
-  htmlOutput("covr_rpt"), 
-  div(print(paste0("Report from {covr} ", meta$covr, 
-                   " created ", meta$when,  
-                   " for {sdtmchecks} ", meta$vers))
-      )
-)
-
-addResourcePath("library", here::here("inst", "dev", "lib"))
-addResourcePath("rpt", here::here("inst", "dev"))
-
-server <- function(input,output){
-  output$covr_rpt <- renderUI({
-    tags$iframe(seamless = "seamless",
-                src= paste0("rpt/", html_rpt_name), 
-                width=1200, height=800)
-  })
-}
-
-shinyApp(ui, server)
+save(html_rpt_run_meta, file = here::here("inst", "dev", "html_rpt_run_meta.RData"), version=2)
