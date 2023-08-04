5429d00ef28be60c049ccf1699476a0d908f6221
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2023-08-05 00:39:30 +0200

    add test scenarios without INV or without OVRLRESP for check_rs_rsdtc_visit_ordinal_error.R

--- tests/testthat/test-check_rs_rsdtc_visit_ordinal_error.R
+++ tests/testthat/test-check_rs_rsdtc_visit_ordinal_error.R
@@ -69,3 +69,42 @@ test_that("Function returns false when expected column not present", {
   
   expect_false(check_rs_rsdtc_visit_ordinal_error(RS))
 })
+
+
+test_that("Function returns false when no Investigator (INV) records", {
+  
+  RS<- data.frame(USUBJID = 101:102,
+                  RSDTC=rep(c("2017-01-01T08:25", "2017-01-05T09:25",
+                              "2017-01-15T10:25","2017-01-20T08:25","2017-01-25T08:25"), 2),
+                  VISITNUM=rep(1:5,2),
+                  VISIT=rep(c("Screening", "Cycle 1", "Cycle 2","Cycle 3","Follow-up"),2),
+                  RSTESTCD="OVRLRESP",
+                  RSEVAL="INDEPENDENT REVIEW FACILITY",
+                  RSEVALID = "RADIOLOGIST 1", 
+                  RSSTAT="",
+                  stringsAsFactors=FALSE)
+  
+  RS$USUBJID = NULL
+  
+  expect_false(check_rs_rsdtc_visit_ordinal_error(RS))
+})
+
+
+
+test_that("Function returns false when Investigator (INV) records but not OVRLRESP", {
+  
+  RS<- data.frame(USUBJID = 101:102,
+                  RSDTC=rep(c("2017-01-01T08:25", "2017-01-05T09:25",
+                              "2017-01-15T10:25","2017-01-20T08:25","2017-01-25T08:25"), 2),
+                  VISITNUM=rep(1:5,2),
+                  VISIT=rep(c("Screening", "Cycle 1", "Cycle 2","Cycle 3","Follow-up"),2),
+                  RSTESTCD="DRCRIND",
+                  RSEVAL="INVESTIGATOR",
+                  RSSTAT="",
+                  stringsAsFactors=FALSE)
+  
+  RS$USUBJID = NULL
+  
+  expect_false(check_rs_rsdtc_visit_ordinal_error(RS))
+})
+
