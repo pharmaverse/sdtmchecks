aa1e9d5fb6e48395b3a2dcbc2fdc95de9078d94a
Wang, Laura {TBB~SOUTH SAN FRANCISCO} <wang.laura@gene.com>
2023-08-17 00:46:45 +0200

    unit tests for: `check_ds_dsdecod_death`, `check_ds_dsdecod_dsstdtc`, `check_ds_dsscat`, `check_ds_dsterm_death_due_to`

---
+++ tests/testthat/test-check_ds_dsscat.R
@@ -0,0 +1,33 @@
+
+test_that("Returns true when no errors present", {
+  DS <- data.frame(
+    USUBJID = c(rep(1,3),rep(2,3),rep(3,3)),
+    DSSCAT= rep(c("STUDY DISCONTINUATION", "ADVERSE EVENT", "PROTOCOL"),3),
+    stringsAsFactors=FALSE
+  )
+  expect_true(check_ds_dsscat(DS))
+})
+
+test_that("Returns false when errors present", {
+  DS <- data.frame(
+    USUBJID = c(rep(1,3),rep(2,3),rep(3,3)),
+    DSSCAT= rep(c("STUDY DISCONTINUATION", "ADVERSE EVENT", "PROTOCOL"),3),
+    stringsAsFactors=FALSE
+  )
+  
+  DS$DSSCAT[8] = "STUDY DISCONTINUATION"
+  expect_false(check_ds_dsscat(DS))
+})
+
+test_that("Returns false when expected column not present", {
+  DS <- data.frame(
+    USUBJID = c(rep(1,3),rep(2,3),rep(3,3)),
+    DSSCAT= rep(c("STUDY DISCONTINUATION", "ADVERSE EVENT", "PROTOCOL"),3),
+    stringsAsFactors=FALSE
+  )
+ 
+  DS$DSSCAT = NULL
+  expect_false(check_ds_dsscat(DS))
+  
+})
+
