3558f23c01214a860a3de820f563154419d7768c
Wang, Laura {TBB~SOUTH SAN FRANCISCO} <wang.laura@gene.com>
2023-08-17 04:31:46 +0200

    added more tests for coverage

--- tests/testthat/test-check_dm_usubjid_ae_usubjid.R
+++ tests/testthat/test-check_dm_usubjid_ae_usubjid.R
@@ -159,3 +159,29 @@ test_that("Returns false when expected column not present - 3", {
   expect_false(check_dm_usubjid_ae_usubjid(DM, AE, DS, EX))
 })
 
+test_that("Returns false when expected column not present - 4", {
+  USUBJID<- c(1:10)
+  DM=data.frame(USUBJID)
+  AE=data.frame(USUBJID)
+  
+  EX <- data.frame(
+    USUBJID = c(1:8, 6, 8, 10, 10, 10, 10),
+    EXOCCUR = rep("Y", times=14),
+    EXDOSE = rep(c(1,2), times=7),
+    EXSTDTC = c(rep("2012-01-01", 10),"2012-02-04","2012-02-04", "", "2012-02-07"),
+    EXTRT = "GDC",
+    stringsAsFactors=FALSE
+  )
+  
+  DS <- data.frame(
+    USUBJID = c(2,8,8),
+    DSDECOD = rep("DEATH", times=3),
+    DSSTDTC = c("2012-12-01", NA, "2013-07-01"),
+    stringsAsFactors=FALSE
+  )
+  
+  DM$USUBJID=NULL
+  
+  expect_false(check_dm_usubjid_ae_usubjid(DM, AE, DS, EX))
+})
+
--- tests/testthat/test-check_ds_dsscat.R
+++ tests/testthat/test-check_ds_dsscat.R
@@ -8,7 +8,7 @@ test_that("Returns true when no errors present", {
   expect_true(check_ds_dsscat(DS))
 })
 
-test_that("Returns false when errors present", {
+test_that("Returns false when errors present - 1", {
   DS <- data.frame(
     USUBJID = c(rep(1,3),rep(2,3),rep(3,3)),
     DSSCAT= rep(c("STUDY DISCONTINUATION", "ADVERSE EVENT", "PROTOCOL"),3),
@@ -16,6 +16,15 @@ test_that("Returns false when errors present", {
   )
   
   DS$DSSCAT[8] = "STUDY DISCONTINUATION"
+  expect_false(check_ds_dsscat(DS))
+})
+
+test_that("Returns false when errors present - 2", {
+  DS <- data.frame(
+    USUBJID = numeric(),
+    DSSCAT= character(),
+    stringsAsFactors=FALSE
+  )
   expect_false(check_ds_dsscat(DS))
 })
 
