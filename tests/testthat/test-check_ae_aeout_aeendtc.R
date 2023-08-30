3b2394dd9ff77945003edd3f1c5b53c56267d83c
lomaxj1 <jennifer.lomax@roche.com>
2023-08-30 14:17:11 +0200

    tests updated to match examples in check script

--- tests/testthat/test-check_ae_aeout_aeendtc.R
+++ tests/testthat/test-check_ae_aeout_aeendtc.R
@@ -1,115 +1,69 @@
 
-test_that("function returns true when no errors are present", {
-
-    AE <- data.frame(
-        USUBJID = 1:8,
-        AEENDTC = c(NA, "NA", "NA",       "",                           "NA",                   "2022-01-05", "2012-05-01",         "1999-01-22"),
-        AEOUT   = c("", "",   "UNKNOWN",  "NOT RECOVERED/NOT RESOLVED", "RECOVERING/RESOLVING", "FATAL",      "RECOVERED/RESOLVED", "RECOVERED/RESOLVED WITH SEQUELAE"),
-        stringsAsFactors = FALSE
-    )
-
-    expect_true(check_ae_aeout_aeendtc(AE))
-
-})
-
 test_that("function returns false when errors are present", {
-
+    
     AE <- data.frame(
-        USUBJID = 1:7,
-        AEENDTC = c("2011-01-01", "2011-01-09", "NA",                         "",                     "NA",   "",                   "2012-05-01"),
-        AEOUT   = c("",           "UNKNOWN",    "NOT RECOVERED/NOT RESOLVED", "RECOVERING/RESOLVING", "FATAL","RECOVERED/RESOLVED", "RECOVERED/RESOLVED WITH SEQUELAE"),
+        USUBJID = 1:10,
+        AETERM  = "AE",
+        AESTDTC = c(NA, "NA", "2015-03-09", "2010-10", "2017-01-20",   "1999-11-02",        "",     NA,                   "2017-08-20",          "2014-12-01"),
+        AEENDTC = c(NA, "NA", "2015-03-12", "2010-10", "2017-01-22",   "1999-11-07",        "",     NA,                   "2017-09-01",          "2015-01-01"),
+        AEOUT   = c("", "",   "",           "",        "NOT RECOVERED","RECOVERED/RESOLVED","FATAL","RECOVERED/RESOLVED", "RECOVERING/RESOLVING","UNKNOWN"),
+        AESPID  = "FORMNAME-R:13/L:13XXXX",
         stringsAsFactors = FALSE
     )
     
     expect_false(check_ae_aeout_aeendtc(AE))
-
-
+    
 })
 
-test_that("Function returns true when no errors are present for an empty dataframe (zero rows)", {
-
-    AE <- data.frame(USUBJID =integer(),
-                     AEENDTC=character(),
-                     AEOUT=character(),
-                     stringsAsFactors=FALSE)
-
-    expect_true(check_ae_aeout_aeendtc(AE))
-})
-
-test_that("Date missing and outcome unknown - returns true", {
-
-    AE <- data.frame(
-        USUBJID = 1,
-        AEENDTC = c(""),
-        AEOUT   = c("UNKNOWN"),
-        stringsAsFactors = FALSE
-    )
-
-    expect_true(check_ae_aeout_aeendtc(AE))
-})
-
-test_that("Date missing and outcome not recovered - returns true", {
+test_that("function returns false when errors are present - with preprocessing", {
     
     AE <- data.frame(
-        USUBJID = 1,
-        AEENDTC = c(""),
-        AEOUT   = c("NOT RECOVERED/NOT RESOLVED"),
+        USUBJID = 1:10,
+        AETERM  = "AE",
+        AESTDTC = c(NA, "NA", "2015-03-09", "2010-10", "2017-01-20",   "1999-11-02",        "",     NA,                   "2017-08-20",          "2014-12-01"),
+        AEENDTC = c(NA, "NA", "2015-03-12", "2010-10", "2017-01-22",   "1999-11-07",        "",     NA,                   "2017-09-01",          "2015-01-01"),
+        AEOUT   = c("", "",   "",           "",        "NOT RECOVERED","RECOVERED/RESOLVED","FATAL","RECOVERED/RESOLVED", "RECOVERING/RESOLVING","UNKNOWN"),
+        AESPID  = "FORMNAME-R:13/L:13XXXX",
         stringsAsFactors = FALSE
     )
     
-    expect_true(check_ae_aeout_aeendtc(AE))
+    expect_false(check_ae_aeout_aeendtc(AE,preproc=roche_derive_rave_row))
+    
 })
 
-test_that("Date missing and outcome unknown - returns true", {
+test_that("function returns false when variable missing", {
     
     AE <- data.frame(
-        USUBJID = 1,
-        AEENDTC = c(""),
-        AEOUT   = c("RECOVERING/RESOLVING"),
+        USUBJID = 1:10,
+        AETERM  = "AE",
+        AESTDTC = c(NA, "NA", "2015-03-09", "2010-10", "2017-01-20",   "1999-11-02",        "",     NA,                   "2017-08-20",          "2014-12-01"),
+        AEENDTC = c(NA, "NA", "2015-03-12", "2010-10", "2017-01-22",   "1999-11-07",        "",     NA,                   "2017-09-01",          "2015-01-01"),
+        AEOUT   = c("", "",   "",           "",        "NOT RECOVERED","RECOVERED/RESOLVED","FATAL","RECOVERED/RESOLVED", "RECOVERING/RESOLVING","UNKNOWN"),
+        AESPID  = "FORMNAME-R:13/L:13XXXX",
         stringsAsFactors = FALSE
     )
-    
-    expect_true(check_ae_aeout_aeendtc(AE))
-})
-
-test_that("Outcome fatal and date missing - returns false", {
-
-    AE <- data.frame(
-        USUBJID = 1,
-        AEENDTC = c(""),
-        AEOUT   = c("FATAL"),
-        stringsAsFactors = FALSE
-    )
-
-    expect_false(check_ae_aeout_aeendtc(AE))
-})
-
-test_that("Outcome recovered and date missing - returns false", {
-    
-    AE <- data.frame(
-        USUBJID = 1,
-        AEENDTC = c(""),
-        AEOUT   = c("FATAL"),
-        stringsAsFactors = FALSE
-    )
+    AE$AEENDTC <- NULL
     
     expect_false(check_ae_aeout_aeendtc(AE))
+    
 })
 
-
-test_that("Function returns the failed object in attr(data)", {
-
+test_that("function returns false when variable missing", {
+    
     AE <- data.frame(
-        USUBJID = 1,
-        AEENDTC = c("2023-03-12"),
-        AEOUT   = c("UNKNOWN"),
+        USUBJID = 1:10,
+        AETERM  = "AE",
+        AESTDTC = c(NA, "NA", "2015-03-09", "2010-10", "2017-01-20",   "1999-11-02",        "",     NA,                   "2017-08-20",          "2014-12-01"),
+        AEENDTC = c(NA, "NA", "2015-03-12", "2010-10", "2017-01-22",   "1999-11-07",        "",     NA,                   "2017-09-01",          "2015-01-01"),
+        AEOUT   = c("", "",   "",           "",        "NOT RECOVERED","RECOVERED/RESOLVED","FATAL","RECOVERED/RESOLVED", "RECOVERING/RESOLVING","UNKNOWN"),
+        AESPID  = "FORMNAME-R:13/L:13XXXX",
         stringsAsFactors = FALSE
     )
-
-    check <- check_ae_aeout_aeendtc(AE)
-
-    expect_true(!is.null(attr(check, "data")))
-
+    AE$AEOUT <- NULL
+    
+    expect_false(check_ae_aeout_aeendtc(AE))
+    
 })
+
 
 
