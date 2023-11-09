e8b73e8ed034427df5631c0ca3959022d1a4e870
harric17 <harris-iii.carl@gene.com>
2023-11-09 18:16:33 +0100

    add spid

--- R/check_ss_ssdtc_dead_ds.R
+++ R/check_ss_ssdtc_dead_ds.R
@@ -8,6 +8,8 @@
 #'
 #' @param SS Subject Status SDTM dataset with variables USUBJID, SSDTC, SSSTRESC, VISIT
 #' @param DS Disposition SDTM dataset with variables USUBJID, DSSTDTC, DSDECOD, DSCAT
+#' @param preproc An optional company specific preprocessing script
+#' @param ... Other arguments passed to methods
 #'
 #' @return boolean value if check failed or passed with 'msg' attribute if the
 #'   test failed
@@ -27,15 +29,17 @@
 #'  VISIT = "FOLLOW-UP"
 #' )
 #'
-#'
 #' DS <- data.frame(
 #'  USUBJID = 1:5,
 #'  DSSTDTC  = c("2020-01-02","2020-01-02", "2020-01-01", "2020-01-03", "2020-01-01"),
 #'  DSDECOD = c(rep('DEATH', 5)),
+#'  DSSPID = "FORMNAME-R:13/L:13XXXX",
 #'  DSCAT   = c("OTHER EVENT", rep("DISPOSITION EVENT", 4))
 #' )
 #'
 #' check_ss_ssdtc_dead_ds(SS, DS)
+#' check_ss_ssdtc_dead_ds(SS, DS, preproc=roche_derive_rave_row)
+#'
 #'
 #' SS <- data.frame(
 #'  USUBJID = 1:5,
@@ -44,15 +48,17 @@
 #'  VISIT = "FOLLOW-UP"
 #' )
 #'
-#'
 #' DS <- data.frame(
 #'  USUBJID = 1:5,
 #'  DSSTDTC  = c("2020-01-02","2020-01-02", "2020-01-01", "2020-01-03", "2020-01-01"),
 #'  DSDECOD = c(rep('DEATH', 5)),
+#'  DSSPID = "FORMNAME-R:13/L:13XXXX",
 #'  DSCAT   = c(rep("DISPOSITION EVENT", 5))
 #' )
 #'
 #' check_ss_ssdtc_dead_ds(SS, DS)
+#' check_ss_ssdtc_dead_ds(SS, DS, preproc=roche_derive_rave_row)
+#'
 #'
 #' SS <- data.frame(
 #'  USUBJID = 1:5,
@@ -61,18 +67,18 @@
 #'  VISIT = "FOLLOW-UP"
 #' )
 #'
-#'
 #' DS <- data.frame(
 #'  USUBJID = 1:5,
 #'  DSSTDTC  = 2,
 #'  DSDECOD = c(rep('DEATH', 5)),
+#'  DSSPID = "FORMNAME-R:13/L:13XXXX",
 #'  DSCAT   = c(rep("DISPOSITION EVENT", 5))
 #' )
 #'
 #' check_ss_ssdtc_dead_ds(SS, DS)
 #'
 
-check_ss_ssdtc_dead_ds <- function(SS, DS) {
+check_ss_ssdtc_dead_ds <- function(SS, DS, preproc=identity,...) {
 
     ###First check that required variables exist and return a message if they don't
     if(SS %lacks_any% c("USUBJID","SSDTC","SSSTRESC", "VISIT")){
@@ -84,9 +90,14 @@ check_ss_ssdtc_dead_ds <- function(SS, DS) {
         fail(lacks_msg(DS, c("USUBJID", "DSSTDTC", "DSDECOD", "DSCAT")))
 
     }else{
+        
+        #Apply company specific preprocessing function
+        DS = preproc(DS,...)
 
         myss <- subset(SS, !is_sas_na(SS$SSDTC) & toupper(SS$SSSTRESC) == 'DEAD', c("USUBJID","SSDTC","SSSTRESC", "VISIT"))
-        myds <- subset(DS, !is_sas_na(DS$DSSTDTC) & toupper(DS$DSDECOD) == 'DEATH' & toupper(DS$DSCAT) == "DISPOSITION EVENT", c("USUBJID", "DSSTDTC", "DSDECOD", "DSCAT"))
+        myds <- subset(DS, !is_sas_na(DS$DSSTDTC) & toupper(DS$DSDECOD) == 'DEATH' & toupper(DS$DSCAT) == "DISPOSITION EVENT") %>% 
+            select(any_of(c("USUBJID", "DSSTDTC", "DSDECOD", "DSCAT","RAVE")))
+        
         mydf <- myss %>%
             left_join(myds, by="USUBJID") %>%
             filter(is_sas_na(DSSTDTC) | SSDTC < DSSTDTC)
