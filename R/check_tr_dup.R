4bc8671573a0351d4ad07b8f9e12c6a1613983c2
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2023-12-20 07:53:45 +0100

    update header, remove dplyr::arrange_at and use group_by_all and n instead

--- R/check_tr_dup.R
+++ R/check_tr_dup.R
@@ -1,14 +1,15 @@
 #' @title Check for duplicate TR records
 #'
 #' @description This check looks for duplicate TR records and returns a data frame.
-#'   Only applies to assessments by investigator.
+#'   Only applies to assessments by Investigator, selected based on uppercased
+#'   TREVAL = "INVESTIGATOR" or missing or TREVAL variable does not exist.
 #'
-#' @param TR TR dataframe with variables USUBJID, TRLINKID, TRTESTCD, TRCAT, TRSTRESC,
-#'           TRSTRESU, VISITDY, TRDTC
+#' @param TR dataframe with variables USUBJID, TRCAT, TRLINKID/TRLNKID, TRTESTCD, TRSTRESC,
+#'           TRDTC, TRSPID (if it exists)
 #'
 #' @author Joel Laxamana
 #'
-#' @importFrom dplyr %>% select filter arrange_at
+#' @importFrom dplyr %>% select filter group_by_all n
 #' @importFrom tidyselect any_of
 #'
 #' @export
@@ -20,55 +21,73 @@
 #'  TRCAT    = c(1,1,2,2),
 #'  TRTESTCD = c(1,1,2,2),
 #'  TRLINKID = c(1,1,2,2),
-#'  TRSPID   = c(1,1,2,2),
-#'  TRDTC    = c(1,1,2,2),
-#'  TRSTRESC = c(1,1,2,2)
+#'  TRDTC = c(rep("2016-01-01",2), rep("2016-06-01",2)),
+#'  TRSTRESC = c(1,1,2,2),
+#'  TRSPID = "FORMNAME-R:19/L:19XXXX",
+#'  TREVAL = "INVESTIGATOR",
+#'  stringsAsFactors = FALSE
 #' )
 #'
 #' check_tr_dup(TR)
-#'
+#' 
+#' TR1 <- TR
+#' TR1$TRSPID <- NULL
+#' 
+#' check_tr_dup(TR1)
+#' 
+#' TR2 <- TR
+#' TR2$TREVAL <- NULL
+#' 
+#' check_tr_dup(TR2)
 #'
 
 check_tr_dup <- function(TR){
+    
     if (TR %lacks_any% c("USUBJID","TRCAT","TRTESTCD","TRDTC","TRSTRESC")){
-
+        
         fail (lacks_msg(TR, c("USUBJID","TRCAT","TRTESTCD","TRDTC","TRSTRESC")))
-
-    } else if (TR %lacks_all% c("TRLINKID","TRLNKID")) {
-
-        fail("TR is missing both the TRLINKID and TRLNKID variable. ")
-
+        
+    } else if (TR %lacks_all% c("TRLINKID", "TRLNKID")) {
+        
+        fail("TR is missing both the TRLINKID and TRLNKID variables. ")
+        
     } else{
-
+        
         myvars <- c("USUBJID","TRCAT","TRTESTCD",names(TR)[names(TR) %in% c("TRLINKID","TRLNKID")],names(TR)[names(TR) %in% "TRSPID"],"TRDTC","TRSTRESC")
-
-
+        
         if(TR %lacks_any% "TREVAL"){
-
-        # leave only variabls on which we want to check duplicate TR records
-        tr1 <- TR %>%
-            arrange_at(myvars) %>%
-            select(any_of(myvars))
-        }else{
+            
+            # for TR domains without TREVAL
+            
+            # leave only variables on which we want to check duplicate TR records
+            # Subsets to duplicated entries only
             tr1 <- TR %>%
-                arrange_at(myvars) %>%
+                select(any_of(myvars)) %>%
+                group_by_all() %>%
+                filter(n()>1)
+            
+        }else{
+            
+            # for TR domains with TREVAL
+            # Subsets to duplicated entries only
+            tr1 <- TR %>%
                 filter(toupper(TREVAL) == "INVESTIGATOR" | is_sas_na(TREVAL)) %>%
-                select(any_of(myvars))
+                select(any_of(myvars)) %>%
+                group_by_all() %>%
+                filter(n()>1)
         }
-
-        # check if there are duplicate TR records
-        dups <- subset(tr1,duplicated(tr1),myvars)
+        
+        # duplicate TR records
+        dups <- subset(tr1, duplicated(tr1), myvars)
         rownames(dups)=NULL
-
-        # declare number of duplicated AEs and print them
+        
+        # declare number of duplicated TR records and print them
         n0 <- paste('There are ', nrow(dups), ' duplicated TR records. ', sep ='')
-
+        
         if (nrow(dups) == 0){
             pass()
         } else{
-            fail(
-                paste(n0), data=dups
-            )
+            fail(paste(n0), data=dups)
         }
     }
 }
