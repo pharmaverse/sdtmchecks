2be64f73184e023afb8bad2992744f9958e457a5
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2023-12-21 22:06:26 +0100

    require >0 records when finding unique pairs

--- R/check_tu_tudtc_across_visit.R
+++ R/check_tu_tudtc_across_visit.R
@@ -34,11 +34,11 @@
 #' check_tu_tudtc_across_visit(TU, preproc=roche_derive_rave_row)
 #'
 #' # no records flagged because non-Investigator results
-#' #TU2 <- TU
-#' #TU2$TUEVAL <- "INDEPENDENT ASSESSOR"
+#' TU2 <- TU
+#' TU2$TUEVAL <- "INDEPENDENT ASSESSOR"
 #'
-#' #check_tu_tudtc_across_visit(TU2)
-#' #check_tu_tudtc_across_visit(TU2, preproc=roche_derive_rave_row)
+#' check_tu_tudtc_across_visit(TU2)
+#' check_tu_tudtc_across_visit(TU2, preproc=roche_derive_rave_row)
 #' 
 #' # example with TUTESTCD and with records flagged
 #' TU3 <- TU
@@ -67,16 +67,16 @@ check_tu_tudtc_across_visit <- function(TU, preproc=identity,...) {
     
     ###First check that required variables exist and return a message if they don't
     if((TU %lacks_any% c("USUBJID", "TUDTC", "VISIT"))){
-
+        
         fail(lacks_msg(TU, c("USUBJID", "TUDTC", "VISIT")))
-
+        
     } else{
         
         #Apply company specific preprocessing function
         TU = preproc(TU,...)
         
         ### Find unique pairs of TUDTC and VISIT per USUBJID
-
+        
         if(TU %lacks_any% "TUEVAL"){
             tusub = TU %>%
                 select(USUBJID, TUDTC, VISIT, any_of(c("TUTESTCD","RAVE")))
@@ -87,30 +87,37 @@ check_tu_tudtc_across_visit <- function(TU, preproc=identity,...) {
         }
         
         tu_orig=tusub #Save RAVE for merging in later
-        tusub = tusub %>% select(-any_of(c("TUTESTCD","RAVE"))) #dont want to unique on RAVE var
-
-        #get unique visit/date pairs per patients
-        mypairs = unique(tusub) 
-        mypairs$x = 1
-
-        ### Get counts of visit values per date for each subject
-        mydf0 = aggregate(mypairs$x,by=list(USUBJID=mypairs$USUBJID, TUDTC=mypairs$TUDTC),FUN=sum)
-
-        ### Subset where count is >1
-        mydf0 = mydf0 %>%
-            filter(x>1) %>%
-            select(USUBJID, TUDTC) 
+        tusub = tusub %>% 
+            select(-any_of(c("TUTESTCD","RAVE"))) #dont want to unique on RAVE var
         
-        mypairs0 = mypairs %>%
-            select(USUBJID, TUDTC,VISIT)
-
-        # subset unique pairs to only instances where visit has >1 date
-        mydf = merge(mydf0,mypairs0,by=c('USUBJID','TUDTC'),all.x = TRUE) %>% 
-            left_join(tu_orig,by=c("USUBJID", "TUDTC", "VISIT")) %>% #merge in RAVE var if it exists
-            unique()
+        if(nrow(tusub)>0){
+            
+            #get unique visit/date pairs per patients
+            mypairs = unique(tusub) 
+            mypairs$x = 1
+            
+            ### Get counts of visit values per date for each subject
+            mydf0 = aggregate(mypairs$x,by=list(USUBJID=mypairs$USUBJID, TUDTC=mypairs$TUDTC),FUN=sum)
+            
+            ### Subset where count is >1
+            mydf0 = mydf0 %>%
+                filter(x>1) %>%
+                select(USUBJID, TUDTC) 
+            
+            mypairs0 = mypairs %>%
+                select(USUBJID, TUDTC,VISIT)
+            
+            # subset unique pairs to only instances where visit has >1 date
+            mydf = merge(mydf0,mypairs0,by=c('USUBJID','TUDTC'),all.x = TRUE) %>% 
+                left_join(tu_orig,by=c("USUBJID", "TUDTC", "VISIT")) %>% #merge in RAVE var if it exists
+                unique()
+            
+            rownames(mydf)=NULL
+        }else{
+            mydf=data.frame()
+        }
         
-        rownames(mydf)=NULL
-
+        
         ### if no consistency
         if (nrow(mydf)==0) {
             pass()
