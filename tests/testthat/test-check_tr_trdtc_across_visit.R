38f0dcb3307480525776a592b8f5cbd21ffd5cd3
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2023-12-21 22:15:43 +0100

    more unit tests for IRF records

--- tests/testthat/test-check_tr_trdtc_across_visit.R
+++ tests/testthat/test-check_tr_trdtc_across_visit.R
@@ -25,6 +25,21 @@ test_that("Returns false when errors present",{
   
 })
 
+
+test_that("Returns true when no errors present among INV records, only errors among IRF present",{
+  
+  TR <- data.frame(
+    USUBJID = 1,
+    TRDTC = c(rep("2016-01-01",3), rep("2016-06-01",5), rep("2016-06-24",2)),
+    VISIT = c(rep("C1D1",3), rep("C1D2",3), rep("C2D1",4)),
+    TRTESTCD = c(rep("LDIAM",7),rep("SAXIS",3)),
+    TREVAL = "INDEPENDENT ASSESSOR",
+    stringsAsFactors=FALSE)
+  
+  expect_true(check_tr_trdtc_across_visit(TR))
+  
+})
+
 test_that("Returns false when expected column not present", {
   
   
