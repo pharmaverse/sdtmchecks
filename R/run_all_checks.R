42a86d1b40a0106dab6769fa3cd421444fcd2c1e
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2024-05-22 17:01:59 +0200

    Dev code removed

--- R/run_all_checks.R
+++ R/run_all_checks.R
@@ -36,7 +36,7 @@
 #'
 #' \dontrun{
 #' 
-#' # Assuming SDTM datasets are in your global environment
+#' # Assuming sdtm datasets are in your global environment
 #' all_rec<-run_all_checks(metads=sdtmchecksmeta, 
 #'                         priority=c("High","Medium","Low"), 
 #'                         type=c("ALL", "ONC", "COVID", "PRO"))
@@ -51,71 +51,60 @@ run_all_checks <- function(metads = sdtmchecksmeta,
                            verbose = TRUE,
                            ncores = 1) {
   
-# temporary
+  if (!is.null(priority) & !all(priority %in% c("High", "Medium", "Low"))) {
+    stop("priority argument should only take values 'High','Medium', or 'Low'")
+  }
+  if (!is.null(type) & !all(type %in% c("ALL", "ONC", "COVID", "PRO", "OPHTH"))) {
+    stop("type argument should only take values 'ALL', 'ONC', 'COVID', 'PRO', 'OPHTH'")
+  }
   
-rlang::env_print(rlang::global_env()) 
-rlang::env_print(rlang::current_env()) 
-
-rlang::env_print(rlang::env_parent(rlang::global_env()))
-rlang::env_print(rlang::env_parent(rlang::current_env()))
-
-
-  #global_env() == current_env() 
-    
-    if (!is.null(priority) & !all(priority %in% c("High", "Medium", "Low"))) {
-        stop("priority argument should only take values 'High','Medium', or 'Low'")
-        }
-    if (!is.null(type) & !all(type %in% c("ALL", "ONC", "COVID", "PRO", "OPHTH"))) {
-        stop("type argument should only take values 'ALL', 'ONC', 'COVID', 'PRO', 'OPHTH'")
-        }
-    
-    # subset meta dataset for specific Type and Priority
-    if (!is.null(priority)) {
-        metads <-subset(metads,metads$priority %in% priority)
-        }
-    
-    if (!is.null(type) ) {
-        metads <-subset(metads, metads$category %in% type)
-        }
-    
-    #Identify missing expected domains to cat out to the user later
-    expected.domains = sort(unique(strsplit(gsub(" ", "", paste(unique(metads$domains),
-                                                                collapse=",")), ",")[[1]]))
-    missing.domains = expected.domains[!(expected.domains %in% ls(envir = .GlobalEnv))]
-    
-    # Run checks
-    
-    all_rec <- parallel::mcmapply(
-        FUN = run_check,
-        metads$check, metads$fxn_in, metads$xls_title, metads$pdf_title, metads$pdf_subtitle, metads$pdf_return, 
-        verbose,
-        SIMPLIFY = FALSE,  # forces mcmapply to return a list
-        USE.NAMES = TRUE,  # generates list names
-        mc.cores = ncores
+  # subset meta dataset for specific Type and Priority
+  if (!is.null(priority)) {
+    metads <-subset(metads,metads$priority %in% priority)
+  }
+  
+  if (!is.null(type) ) {
+    metads <-subset(metads, metads$category %in% type)
+  }
+  
+  #Identify missing expected domains to cat out to the user later
+  expected.domains = sort(unique(strsplit(gsub(" ", "", paste(unique(metads$domains),
+                                                              collapse=",")), ",")[[1]]))
+  missing.domains = expected.domains[!(expected.domains %in% ls(envir = .GlobalEnv))]
+  
+  # Run checks
+  
+  all_rec <- mcmapply(
+    FUN = run_check,
+    metads$check, metads$fxn_in, metads$xls_title, metads$pdf_title, metads$pdf_subtitle, metads$pdf_return, 
+    verbose,
+    SIMPLIFY = FALSE,  # forces mcmapply to return a list
+    USE.NAMES = TRUE,  # generates list names
+    mc.cores = ncores
+  )
+  
+  if (verbose == TRUE & length(missing.domains) > 0) {
+    cat(
+      paste0(
+        "\nDomain not found: ",
+        missing.domains,
+        ". Checks requiring this domain were not run."
+      )
     )
-    
-    if (verbose == TRUE & length(missing.domains) > 0) {
-        cat(
-            paste0(
-                "\nDomain not found: ",
-                missing.domains,
-                ". Checks requiring this domain were not run."
-            )
-        )
-    }
-    
-    cat("\n")
-    
-    if(!("covid_df" %in% ls(envir = .GlobalEnv))){
-      warning("An object named 'covid_df' was not found in your global environment so the following checks were not run: 
+  }
+  
+  cat("\n")
+  
+  if(!("covid_df" %in% ls(envir = .GlobalEnv))){
+    warning("An object named 'covid_df' was not found in your global environment so the following checks were not run: 
               check_ae_aeacn_ds_disctx_covid
               check_ae_aeacnoth_ds_stddisc_covid
               check_dv_ae_aedecod_covid")
-    }
-    
+  }
+  
   cat("\n")
-    
+  
   # return the final list
   return(all_rec)
-    
+  
 } # end of function
