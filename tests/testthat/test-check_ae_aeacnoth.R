de2ecd327597f2a892a5c3f6740e8f5248ee276b
Bodach, Sara {MDBB~South San Francisco} <bodach.sara@gene.com>
2024-05-23 22:23:03 +0200

    add header examples, minor return message update

--- tests/testthat/test-check_ae_aeacnoth.R
+++ tests/testthat/test-check_ae_aeacnoth.R
@@ -49,6 +49,38 @@ test_that("Function returns false when errors are present", {
 
 
 
+test_that("Function returns false when errors are present", {
+    
+    AE <- data.frame(
+        USUBJID = 1:7,
+        AETERM = 1:7,
+        AESTDTC = 1:7,
+        AEACNOTH = 1:7,
+        AEACNOT1 = 1:7,
+        AEACNOT2 = 1:7,
+        AESPID = "FORMNAME-R:13/L:13XXXX"
+    )
+    
+    AE$AEACNOTH[1] = NA
+    AE$AEACNOT1[1] = NA
+    AE$AEACNOT2[1] = NA
+    AE$AEACNOTH[2] = "MULTIPLE"
+    AE$AEACNOT1[2] = "DOSE REDUCED"
+    AE$AEACNOT2[2] = "DRUG WITHDRAWN"
+    AE$AEACNOTH[3] = "MULTIPLE"
+    AE$AEACNOT1[3] = "DOSE REDUCED"
+    AE$AEACNOT2[3] = NA
+    AE$AEACNOTH[4] = "MULTIPLE"
+    AE$AEACNOT1[4] = NA
+    AE$AEACNOT2[4] = "DRUG WITHDRAWN"
+    AE$AEACNOTH[5] = "MULTIPLE"
+    AE$AEACNOT1[5] = NA
+    AE$AEACNOT2[5] = NA
+    
+    expect_false(check_ae_aeacnoth(AE))
+})
+
+
 test_that("Function returns false when errors are present - with preprocessing function specified", {
     
     AE <- data.frame(
