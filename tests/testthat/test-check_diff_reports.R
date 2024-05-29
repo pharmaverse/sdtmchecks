7e1e75edfb166bc21c83d681544ee70013f6a203
jimrothstein <jimrothstein@gmail.com>
2024-05-29 15:01:35 -0700

    295_place_holder

---
+++ tests/testthat/test-check_diff_reports.R
@@ -0,0 +1,11 @@
+
+
+# See sdtmchecks::diff_reports  documentation
+
+test_that("diff_reports correctly shows no differences when old and new reports are identical",{
+  #res = diff_reports(old_report = old, new_reports =  new) 
+  expect_true(TRUE)
+})
+
+# and many more tests ...
+
